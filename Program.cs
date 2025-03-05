6e31f7ae5eeba1627cc00b7ad0e8cdd90ab646e3
NathanBLsoftwares <nathan@blsoftwares.com.br>
2025-03-05 15:34:01 -0300

    Aprimorando para o uso de classes

--- Program.cs
+++ Program.cs
@@ -1,122 +1,73 @@
-﻿using System;
-using Microsoft.VisualBasic;
 using Microsoft.Win32;
+using Registro_de_Campos;
 
 class Programa
 {
     static void Main()
     {
+        // Chama o método para detectar as versões e exibi-las.
+        List<SoftwareVersao> lista = DetectarVersao();
 
-        ///Verificação das versões do AutoCad
-        RegistryKey? chaveRegistro = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\AUTODESK\AUTOCAD");
-        if(chaveRegistro != null)
-        {
-            string[] nomesSubChaves = chaveRegistro.GetSubKeyNames();
-            foreach(string CadVersion in nomesSubChaves)
+        // Exibe as versões de cada software
+        foreach (var software in lista)
             {
-                switch(CadVersion)
+            switch (CadVersion)
                 {
-                    case "R21.0" :
+                case "R21.0":
                         Console.WriteLine("\t AutoCad -> 2017");
                         break;
-                    case "R22.0" :
+                case "R22.0":
                         Console.WriteLine("\t AutoCad -> 2018");
                         break;
-                    case "R23.0" :
+                case "R23.0":
                         Console.WriteLine("\t AutoCad -> 2019");
                         break;
-                    case "R23.1" :
+                case "R23.1":
                         Console.WriteLine("\t AutoCad -> 2020");
                         break;
-                    case "R24.0" :
+                case "R24.0":
                         Console.WriteLine("\t AutoCad -> 2021");
                         break;
-                    case "R24.1" :
+                case "R24.1":
                         Console.WriteLine("\t AutoCad -> 2022");
                         break;
-                    case "R24.2" :
+                case "R24.2":
                         Console.WriteLine("\t AutoCad -> 2023");
                         break;
-                    case "R24.3" :
+                case "R24.3":
                         Console.WriteLine("\t AutoCad -> 2024");
                         break;
-                    case "R25.0" :
+                case "R25.0":
                         Console.WriteLine("\t AutoCad -> 2025");
                         break;
                     default:
                         break;
                 }
-            
             }
-        
-        }else
-        {
-            Console.WriteLine("Nenhuma versão encontrada do AUTOCAD");
         }
 
-        //Verificação das versões do BRICSCAD
-        //Abrir chave de rigistro
-        chaveRegistro = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\BRICYSOFT\BRICSCAD");
-        if(chaveRegistro != null)
+    public static void DetectarVersao(List<SoftwareVersao> lista, string chave, string software)
         {
-            string [] nomesSubChaves = chaveRegistro.GetSubKeyNames();
-            Console.WriteLine("****************");
-            Console.WriteLine("VERSÕES BRICSCAD");
-            Console.WriteLine("****************");
-            foreach(string subChaves in nomesSubChaves)
+        RegistryKey chaveRegistro = Registry.LocalMachine.OpenSubKey(chave);
+        if (chaveRegistro != null)
             {
-                Console.WriteLine($"\tBRICSAD versão -> {subChaves}" );
+            string[] nomesSubChaves = chaveRegistro.GetSubKeyNames();
+            foreach (string nome in nomesSubChaves)
+            {
+                lista.Add(new SoftwareVersao { Software = software, Versao = nome, Chave = $"{chave}\\{nome}" });
+                Console.WriteLine($"ZWcad Versão: {nome}");
             }
             chaveRegistro.Close();
         }
-        else
-        {
-            Console.WriteLine("Nenhuma Versão encontrada do BRICSCAD");
         }
 
-
-
-
-        //Verificação das versões do GSTARCAD
-        //Abrir chave de rigistro
-        chaveRegistro = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\GSTARSOFT\GSTARCAD");
-        if(chaveRegistro != null)
+    public static List<SoftwareVersao> DetectarVersao()
         {
-            string [] nomesSubChaves = chaveRegistro.GetSubKeyNames();
-            Console.WriteLine("****************");
-            Console.WriteLine("VERSÕES GSTARCAD");
-            Console.WriteLine("****************");
-            foreach(string subChaves in nomesSubChaves)
-            {
-                Console.WriteLine($"\tGSTARCAD versão -> {subChaves}" );
-            }
-            chaveRegistro.Close();
-        }
-        else
-        {
-            Console.WriteLine("Nenhuma Versão encontrada do GSTARCAD");
-        }
+        List<SoftwareVersao> lista = new List<SoftwareVersao>();
+        DetectarVersao(lista, @"SOFTWARE\ZWSOFT\ZWCAD", "Zwcad");
+        DetectarVersao(lista, @"SOFTWARE\GSTARSOFT\GSTARCAD", "Gstarcad");
+        DetectarVersao(lista, @"SOFTWARE\BRICYSOFT\BRICSCAD", "Bricscad");
 
-        //Verificação das versões do ZWCAD
-        //Abrir chave de rigistro
-        chaveRegistro = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\ZWSOFT\ZWCAD");
-        if(chaveRegistro != null)
-        {
-            string [] nomesSubChaves = chaveRegistro.GetSubKeyNames();
-            Console.WriteLine("*************");
-            Console.WriteLine("VERSÕES ZWCAD");
-            Console.WriteLine("**************");
-            foreach(string subChaves in nomesSubChaves)
-            {
-                Console.WriteLine($"\tZWCAD versão -> {subChaves}" );
-            }
-            chaveRegistro.Close();
+        return lista;
         }
-        else
-        {
-            Console.WriteLine("Nenhuma Versão encontrada do ZWCAD");
         }
-
-
-    }
-}
